07c2913d45cea1670b8568451ea5a2fb35cae7cf
J3ss62205 <147669349+J3ss62205@users.noreply.github.com>
2023-12-06 17:37:57 -0800

    I forgot, a lot

--- Group Project/src/cs141/KaiDeckerAndJessicaHenry/Main.java
+++ Group Project/src/cs141/KaiDeckerAndJessicaHenry/Main.java
@@ -1,23 +1,19 @@
 package cs141.KaiDeckerAndJessicaHenry;
 import java.util.*;
+//Name: Jessica Henry
+
 
 public class Main {
-//	0: Create a Booking -----FIX ARRAY
-//	1: Remove a Booking ---- FIX ARRAY
-//	2: Update Rooms Information ---FIX ARRAY
-//	3: Find Someone in the System ---- HELPPPPPPPPPPP
-//	4: Check Availbity ----- DONE
-//	5: Remove a Room -----FIX ARRAY
-//	6: Add a Room ------- FIX ARRAY
-//	7: Print Out All or Open Rooms ------------ NOT DONE
-//	8: Quit ----------DONE
 
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
+		
 		int choice;
 		int room;
+		//Variables yayyyyyyyyy
 		
 		HotelManager hotel = new HotelManager();
+		//This will create an object and call the constructor 
 		
 		do {
 			System.out.println("0: Create a Booking");
@@ -32,6 +28,7 @@ public class Main {
 			//This is the menu 
 			
 			choice = scnr.nextInt();
+			//This gets the users input
 			
 			switch(choice){
 				case 0:
@@ -100,6 +97,7 @@ public class Main {
 						
 						int index = hotel.getRegIndex(name);
 						hotel.updateRegRoom(index);
+						//This calls the method
 					}
 					else if (room == 2) {
 						System.out.println("What is the name you booked with: ");
@@ -108,6 +106,7 @@ public class Main {
 						
 						int index = hotel.getLuxIndex(name);
 						hotel.updateLuxRoom(index);
+						//Once again calls the method
 					}
 					break;
 					
@@ -116,6 +115,7 @@ public class Main {
 					String name = scnr.nextLine();
 					
 					hotel.findSomeone(name);
+					//This can find someone in the system
 					
 					break;
 				case 4:
@@ -154,19 +154,32 @@ public class Main {
 					
 					if (room == 1) {						
 						hotel.addRegRoom();
+						//This'll add a regular room
 					}
 					else if (room == 2) {
 						hotel.addLuxRoom();
+						//This'll add a luxury room 
 					}
 					
 					break;
 				case 7:
+					System.out.println("Would you like to print out all or open rooms?");
+					System.out.println("1: All");
+					System.out.println("2: Open");
+					room = scnr.nextInt();
 					
+					if (room == 1) {						
+						hotel.printAll();
+						//This calls the print all method 
+					}
+					else if (room == 2) {
+						hotel.printOpen();
+						//This calls the printing open rooms method 
+					}
 					break;
 			}
 			
-			
-		} while (choice != 8);
+		} while (choice != 8); //If they type 8 they will be out of loop 
 	}
 
 }
