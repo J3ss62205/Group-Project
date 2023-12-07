59e339cfc22bd3b109892b4505e7cd7bd3914433
kai-a-d <151413828+kai-a-d@users.noreply.github.com>
2023-12-07 15:12:52 -0800

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,3 +1,12 @@
+/////
+//	Name: Kai Decker
+//	Class Name: Room
+//
+//	Description: This class is meant to use an abstract class to make a uniform model a room
+//	that is meant to be expanded upon inside a subclass through the use of inheritance.
+//
+/////
+
 /**
  * 
  */
@@ -7,13 +16,14 @@ package cs141.KaiDeckerAndJessicaHenry;
  * 
  */
 public abstract class Room {
-	
+	// Fill in room with information.
 	abstract void createBooking(String name, int occ);
+	// Fill in room with information stating it is empty.
 	abstract void removeBooking();
+	// Allow the room's information to be changed.
 	abstract void updateRoom();
+	// Output room information.
 	abstract void print();
+	// print information for open rooms only.
 	abstract void printOpen();
-	abstract boolean paid();
-
-	
 }
